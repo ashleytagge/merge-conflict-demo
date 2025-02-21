f4753f51b25b8bdfbf5402b8784527efb6dd9747
Megan Loertscher <pearl@loertscher.org>
2025-02-21 16:14:12 -0700

    fix: breaking changes

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
+    <h1 class="p-4 text-3xl font-bold text-green-400 bg-sky-200 h-screen">
+      Hello end users!
     </h1>
   </div>
 </template>
+<script setup lang="ts">
+</script>
